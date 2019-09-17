eb0f14022caa6a188147f1a8f4441ec066b4caeb
Ulfur_Orn <ulfurinn@gmail.com>
2019-09-17 22:15:33 +0000

    Program that creates maze started

---
+++ Maze_Creator.py
@@ -0,0 +1,10 @@
+maze = [[0 for i in range(5)] for i in range(5)]
+counter = 0
+
+# for pos_x in range(5):
+#     for pos_y in range(5):
+#         counter += 1
+#         maze[pos_y][pos_x] = counter
+
+for i in range(len(maze)):
+    print(maze[i])

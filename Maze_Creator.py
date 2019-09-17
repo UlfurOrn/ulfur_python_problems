49357e98b9a2d1bdea6220e51c16c135ec14cb5f
Ulfur_Orn <ulfurinn@gmail.com>
2019-09-17 22:50:43 +0000

    Algorithm and start of program

--- Maze_Creator.py
+++ Maze_Creator.py
@@ -1,10 +1,24 @@
+"""
+Create grid
+    while
+        1. From any given point in grid find random empty neighbor
+        2. remove wall
+        3. add current position to list
+        4. move to new position
+    else
+        1. If no empty neighbor move to position latest in stack
+        2. Remove old position from stack
+"""
+
+pos_x = 0; pos_y = 0
+
 maze = [[0 for i in range(5)] for i in range(5)]
-counter = 0
 
-# for pos_x in range(5):
-#     for pos_y in range(5):
-#         counter += 1
-#         maze[pos_y][pos_x] = counter
+if 1 in maze:
+    print(True)
+maze[pos_y][pos_x] = 1
+if 1 in maze:
+    print(True)
 
-for i in range(len(maze)):
+for i in range(5):
     print(maze[i])
